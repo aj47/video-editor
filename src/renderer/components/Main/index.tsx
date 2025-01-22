66892f7cbf3c476ea9dac8120ba8b5c7fc1316c9
AJ <yspdev@gmail.com>
2025-01-22 13:39:20 -0800

    refactor: Remove unused TimelineEditor component from Main

--- src/renderer/components/Main/index.tsx
+++ src/renderer/components/Main/index.tsx
@@ -3,7 +3,6 @@ import { useFileInputController } from '@hooks/use-file-input-controller';
 import { ConvertStatusModal } from './ConvertStatusModal';
 import { DropNavigationModal } from './DropNavigationModal';
 import { Editor } from './Editor';
-import { TimelineEditor } from './TimelineEditor';
 import * as Styled from './Styled';
 
 export const Main = () => {
@@ -12,7 +11,6 @@ export const Main = () => {
   return (
     <Styled.Container {...(getRootProps() as any)}>
       <Editor />
-      <TimelineEditor />
 
       <ConvertStatusModal />
       <DropNavigationModal isVisible={isDragActive} />
